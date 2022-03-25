203723aa281f0b1650931ba41420b48b530df154
vishaldotgarwal@gmail.com <vishaldotgarwal@gmail.com>
2022-03-25 12:34:42 +1030

    this os

--- task3.js
+++ task3.js
@@ -22,6 +22,23 @@ function displayBlog(){
     const createpost = document.createElement('P');
     createpost.className = "post-content";
     createpost.innerText = str;
+    var elements = document.querySelectorAll('input[name = "color"]');
+    let colour = "black";
+    for(let i = 0; i < elements.length; i++){
+        if(elements[i].checked === true){
+             colour = elements[i].value;
+        }
+    }
+    createpost.style.color = colour;
     parent.appendChild(createpost);
+}
 
+
+
+function displayBlog_manytimes(){
+    var element = document.querySelector('input[name = "quantity"]');
+    var storeValue = element.value;
+    for(let i = 0; i < storeValue; i++){
+        displayBlog();
+    }
 }
